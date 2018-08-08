45318b40676c68444f00b7707ff4c56dca3ae867
lvkv <lukas@rytas.io>
2018-08-08 11:54:17 -0400

    bg color, 1/2 working autoscroll, formatting

--- scripts/autoscroll.js
+++ scripts/autoscroll.js
@@ -2,10 +2,11 @@
 var IDarray = ["#admitted", "#bakesale", "#wtchs", "#chs", "#makers", "#fresh", "#nhsp", "#dnalc", "#smithtown", "#ecolibuds", "#lavalle", "#girlsinc", "#stepbnl", "#istem"]
 
 for(var i=0; i<IDarray.length; i++){
-  var imgID = IDarray[i] + "img";
+  var section = IDarray[i];
+  var imgID = section + "img";
+  console.log(imgID);
+  console.log(section)
   $(imgID).click(function() {
-      $('html,body').animate({
-          scrollTop: $(IDarray[i]).offset().top},
-          'slow');
+      $('html,body').animate({scrollTop: $(section).offset().top}, 'slow');
   });
 }
