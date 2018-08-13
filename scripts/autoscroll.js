b5a76c780fc352fed9f6d7a7438e92735f507c0a
lvkv <lukas@rytas.io>
2018-08-13 11:21:38 -0400

    Goodbye Anime.js. It was a good run.

--- scripts/autoscroll.js
+++ scripts/autoscroll.js
@@ -1,7 +1,6 @@
 // The IDs of each section on the page
 var IDarray = ["#admitted", "#bakesale", "#wtchs", "#chs", "#makers", "#fresh", "#nhsp", "#dnalc", "#smithtown", "#ecolibuds", "#lavalle", "#girlsinc", "#stepbnl", "#istem"]
-// var section = IDarray[0];
-// var imgID = section + "img";
+
 for(var i=0; i<IDarray.length; i++){
   let section = IDarray[i]; //  Current section ID
   let imgID = section + "img"; // The ID of the grid image representing the current section
